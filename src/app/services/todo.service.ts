44b4b8a036eb01df1f976b11d5b0b1f0094cf331
brownb202 <brownb202@gmail.com>
2021-01-14 19:53:31 -0700

    added jsonplaceholder for pulling example todos, added limit of 5

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,30 +1,19 @@
 import { Injectable } from '@angular/core';
+import  { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Todo } from '../models/Todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = "https://jsonplaceholder.typicode.com/todos?_limit=5";
+  todosLimit = '?_limit=5'
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: "Todo one",
-        completed: false,
-      },
-      {
-        id: 2,
-        title: "Todo two",
-        completed: true,
-      },
-      {
-        id: 2,
-        title: "Todo three",
-        completed: false,
-      },
-
-    ]
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 }
